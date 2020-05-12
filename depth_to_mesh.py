08db53597f1edc135eae2538b10a485f3b349e79
hesom <5159852+hesom@users.noreply.github.com>
2020-05-12 13:04:08 +0200

    Add progress bar

--- depth_to_mesh.py
+++ depth_to_mesh.py
@@ -8,6 +8,7 @@ import numpy as np
 import math
 import logging
 from skimage.io import imread
+from tqdm import tqdm
 
 DEFAULT_CAMERA = o3d.camera.PinholeCameraIntrinsic(
     width=640, height=480,
@@ -68,7 +69,7 @@ def depth_to_mesh(depth, camera=DEFAULT_CAMERA, minAngle=3.0):
     :returns: an open3d.geometry.TriangleMesh containing the convertes mesh
     """
     
-    logger.debug('Reprojecting points...')
+    logger.info('Reprojecting points...')
     K = camera.intrinsic_matrix
     K_inv = np.linalg.inv(K)
     pixel_coords = pixel_coord_np(depth.shape[1], depth.shape[0])
@@ -78,41 +79,43 @@ def depth_to_mesh(depth, camera=DEFAULT_CAMERA, minAngle=3.0):
     w = camera.width
     h = camera.height
 
-    for i in range(0, h-1):
-        for j in range(0, w-1):
-            verts = [
-                cam_coords[:, w*i+j],
-                cam_coords[:, w*(i+1)+j],
-                cam_coords[:, w*i+(j+1)],
-            ]
-            if [0,0,0] in map(list, verts):
-                continue
-            v1 = verts[0] - verts[1]
-            v2 = verts[0] - verts[2]
-            n = np.cross(v1, v2)
-            n /= np.linalg.norm(n)
-            center = (verts[0] + verts[1] + verts[2]) / 3.0
-            u = center / np.linalg.norm(center)
-            angle = math.degrees(math.asin(abs(np.dot(n, u))))
-            if angle > minAngle:
-                indices.append([w*i+j, w*(i+1)+j, w*i+(j+1)])
+    with tqdm(total=(h-1)*(w-1)) as pbar:
+        for i in range(0, h-1):
+            for j in range(0, w-1):
+                verts = [
+                    cam_coords[:, w*i+j],
+                    cam_coords[:, w*(i+1)+j],
+                    cam_coords[:, w*i+(j+1)],
+                ]
+                if [0,0,0] in map(list, verts):
+                    continue
+                v1 = verts[0] - verts[1]
+                v2 = verts[0] - verts[2]
+                n = np.cross(v1, v2)
+                n /= np.linalg.norm(n)
+                center = (verts[0] + verts[1] + verts[2]) / 3.0
+                u = center / np.linalg.norm(center)
+                angle = math.degrees(math.asin(abs(np.dot(n, u))))
+                if angle > minAngle:
+                    indices.append([w*i+j, w*(i+1)+j, w*i+(j+1)])
 
-            verts = [
-                cam_coords[:, w*i+(j+1)],
-                cam_coords[:, w*(i+1)+j],
-                cam_coords[:, w*(i+1)+(j+1)],
-            ]
-            if [0,0,0] in map(list, verts):
-                continue
-            v1 = verts[0] - verts[1]
-            v2 = verts[0] - verts[2]
-            n = np.cross(v1, v2)
-            n /= np.linalg.norm(n)
-            center = (verts[0] + verts[1] + verts[2]) / 3.0
-            u = center / np.linalg.norm(center)
-            angle = math.degrees(math.asin(abs(np.dot(n, u))))
-            if angle > minAngle:
-                indices.append([w*i+(j+1),w*(i+1)+j, w*(i+1)+(j+1)])
+                verts = [
+                    cam_coords[:, w*i+(j+1)],
+                    cam_coords[:, w*(i+1)+j],
+                    cam_coords[:, w*(i+1)+(j+1)],
+                ]
+                if [0,0,0] in map(list, verts):
+                    continue
+                v1 = verts[0] - verts[1]
+                v2 = verts[0] - verts[2]
+                n = np.cross(v1, v2)
+                n /= np.linalg.norm(n)
+                center = (verts[0] + verts[1] + verts[2]) / 3.0
+                u = center / np.linalg.norm(center)
+                angle = math.degrees(math.asin(abs(np.dot(n, u))))
+                if angle > minAngle:
+                    indices.append([w*i+(j+1),w*(i+1)+j, w*(i+1)+(j+1)])
+                pbar.update(1)
     
     points = o3d.utility.Vector3dVector(cam_coords.transpose())
 
