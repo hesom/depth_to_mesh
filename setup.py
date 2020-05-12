845f4d6ea5cefa7561d3d91abf80ff7cc10e8214
hesom <5159852+hesom@users.noreply.github.com>
2020-05-12 13:28:44 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='depth-to-mesh',
+    version='0.1',
+    description='Converts depth maps into triangle meshes',
+    url='https://github.com/hesom/depth_to_mesh',
+    author='Hendrik Sommerhoff',
+    author_email='sommerhoff.hendrik@gmail.com',
+    license='MIT',
+    packages=['depth_to_mesh'],
+    install_requires=[
+        'open3d',
+        'numpy',
+        'scikit-image',
+        'tqdm'
+    ],
+    zip_safe=False
+)
